adb926abb6ec4f10d7c5e26b9334480c7ccea7f3
Ash Sendell-Price <36015770+ASendellPrice@users.noreply.github.com>
2023-02-24 13:06:57 +0000

    Update 04_apply_hard_filters.sh

--- 04_apply_hard_filters.sh
+++ 04_apply_hard_filters.sh
@@ -39,9 +39,6 @@ java -jar ../BIN/picard.jar MergeVcfs \
   I=called_by_GATK_biallelic.vcf.gz \
   O=called_by_GATK_invariant_plus_biallelic.vcf.gz
 
-# Remove non-combined VCFs
-rm called_by_GATK_invariant.vcf.gz rm called_by_GATK_biallelic.vcf.gz
-
 # Remove sites where parental genotypes are missing as these aren't informative.
 zcat called_by_GATK_invariant_plus_biallelic.vcf.gz \
   | bcftools view -e 'GT[9]="mis" || GT[10]="mis"' \
@@ -51,5 +48,12 @@ zcat called_by_GATK_invariant_plus_biallelic.vcf.gz \
 bedtools intersect -a called_by_GATK_invariant_plus_biallelic.informative_sites.vcf.gz \
   -b ../resources/HiglyMappable_Unmasked_ranges.txt -header \
   | bgzip > called_by_GATK_invariant_plus_biallelic.informative_sites.HighlyMappable.NonRepeat.vcf.gz
+
+# Set sample genotypes with GQ < 20 to missing "./." and again 
+# remove any sites where parental genotypes are missing
+zcat called_by_GATK_invariant_plus_biallelic.informative_sites.HighlyMappable.NonRepeat.vcf.gz \
+  | bcftools filter -S . -e 'FMT/GQ[@all.samples] < 20' \
+  | bcftools view -e 'GT[9]="mis" || GT[10]="mis"' \
+  | bgzip > called_by_GATK_invariant_plus_biallelic.informative_sites.HighlyMappable.NonRepeat.minGQ20.vcf.gz
 
 # END!
